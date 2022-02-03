0a02e6f94688e190dc9d0b9a1986e16d5eb4b0d5
Blackraven93 <reblackraven@gmail.com>
2022-02-03 19:01:40 +0900

    Feat: connect mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,30 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot(),
+    UsersModule,
+    MongooseModule.forRoot(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  private readonly isDev: boolean = process.env.MODE === 'dev' ? true : false;
+  configure(consumer: MiddlewareConsumer) {
+    // loggerMiddleware binding
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+    // mongoose query
+    mongoose.set('debug', this.isDev);
+  }
+}
