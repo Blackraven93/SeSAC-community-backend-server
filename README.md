0dd8c12684e73bf3ac156337af0c9e591d18d418
kwak se jin <Reblackraven@gmail.com>
2022-02-08 13:10:45 +0900

    Feat: set http method in controller

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # SeSAC Community Backend Server
 
 - Nest.js를 이용해 SeSAC Community를 제작하는 레파지토리입니다. 
+
 ## 프로젝트 기술스택
 
 **백엔드**
@@ -11,7 +12,7 @@
 
 **인프라**
 
-- AWS EC2
+- AWS lightsails
 - AWS S3
 
 ### 핵심 기능
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) { }
+
+  // users//all
+  @Get()
+  getAllUser(): string {
+    return `all Users`;
+  }
+
+  //users/:id
+  @Get(':id')
+  getUser() {
+    return 'get user';
+  }
+
+  // 회원가입
+  @Post()
+  createUser() {
+    return 'create user';
+  }
+
+  // 교체
+  @Put(':id')
+  updateUser() {
+    return 'update user';
+  }
+
+  // 부분 수정
+  @Patch('id')
+  updatePartialUser() {
+    return;
+  }
+
+  @Delete(':id')
+  deleteUser() {
+    return;
+  }
+}
